b04ee1a4d8f5b2c9df11680d0ccfa94f0e320529
adrrike <84742544+adrrike@users.noreply.github.com>
2021-11-25 13:23:29 -0500

    Add files via upload
    Dropdowns working. Year slider still is not.

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ import pandas as pd
 app = dash.Dash()   
 
 #reading joined netflix dataset 
-df = pd.read_csv('../Netflix_Team_1/Resources/joined_df.csv', low_memory=False)
+df = pd.read_csv('../Netflix_Team_1/Resources/netflix_updated_with_genres.csv', low_memory=False)
 
-available_indicators = df['Type'].unique()
+available_indicators = df.columns.unique()
 
 app.layout = html.Div([
     html.Div([
@@ -24,27 +24,15 @@ app.layout = html.Div([
             dcc.Dropdown(
                 id='xaxis-column',
                 options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Genre'
-            ),
-            dcc.RadioItems(
-                id='xaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
+                value='language'
+            ) 
         ], style={'width': '48%', 'display': 'inline-block'}),
 
         html.Div([
             dcc.Dropdown(
                 id='yaxis-column',
                 options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Language'
-            ),
-            dcc.RadioItems(
-                id='yaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
+                value='rating'
             )
         ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
     ]),
@@ -53,10 +41,10 @@ app.layout = html.Div([
 
     dcc.Slider(
         id='year--slider',
-        min=df['Start Year'].min(),
-        max=df['Start Year'].max(),
-        value=df['Start Year'].max(),
-        marks={str(year): str(year) for year in df['Start Year'].unique()},
+        min=df['start_year'].min(),
+        max=df['start_year'].max(),
+        value=df['start_year'].max(),
+        marks={str(year): str(year) for year in df['start_year'].unique()},
         step=None
     )
 ])
@@ -66,25 +54,26 @@ app.layout = html.Div([
     Output('indicator-graphic', 'figure'),
     Input('xaxis-column', 'value'),
     Input('yaxis-column', 'value'),
-    Input('xaxis-type', 'value'),
-    Input('yaxis-type', 'value'),
     Input('year--slider', 'value'))
-def update_graph(xaxis_column_name, yaxis_column_name,
-                 xaxis_type, yaxis_type,
-                 year_value):
-    dff = df[df['Start Year'] == year_value]
 
-    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-                     y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-                     hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Title'])
+def update_graph(xaxis_column_name, yaxis_column_name, year_value):
+    
+    # year_value = df['start_year']
+    dff = df[df['start_year'] == year_value]
 
-    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
 
-    fig.update_xaxes(title=xaxis_column_name,
-                     type='linear' if xaxis_type == 'Linear' else 'log')
+    fig = px.scatter(
+                    x=df['{}'.format(xaxis_column_name)],
+                    y=df['{}'.format(yaxis_column_name)],
+                    hover_name = df['title'])
 
-    fig.update_yaxes(title=yaxis_column_name,
-                     type='linear' if yaxis_type == 'Linear' else 'log')
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, 
+                    hovermode='closest')
+    fig.update_layout(title = 'Interactive Graph')
+
+    fig.update_xaxes(title=xaxis_column_name)
+
+    fig.update_yaxes(title=yaxis_column_name)
 
     return fig
 
